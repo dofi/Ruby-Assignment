f04b2e6e4ac4f951e5e3f8d4f61626572429c4b9
dofi <douglas.fitzpatrick@colorado.edu>
2014-02-05 18:24:04 -0400

    Create 1_helloworld.rb

---
+++ 1_helloworld.rb
@@ -0,0 +1,12 @@
+# Part1: Hello World
+class HelloWorldClass
+    def initialize(name)
+       @name = name.capitalize
+    end
+    def sayHi
+        puts "Hello #{@name}!"
+    end
+end
+hello = HelloWorldClass.new("doug fitzpatrick")
+hello.sayHi
+
